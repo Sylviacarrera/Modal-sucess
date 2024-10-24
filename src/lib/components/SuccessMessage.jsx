e6344f813cfd493ef34c0b2378be37c06e1ec5e9
Sylviacarrera <129325236+Sylviacarrera@users.noreply.github.com>
2024-10-24 10:03:28 +0200

    push lib

---
+++ src/lib/components/SuccessMessage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../components/SuccessMessage.scss'; 
+
+const ModalSuccessMessage = ({ isDisplayed, onClose, message }) => {
+  // Fonction pour fermer la modale
+  const handleOverlayClick = (e) => {
+    if (e.target.className.includes('modal-overlay')) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className={`modal-overlay ${isDisplayed ? 'show' : ''}`} onClick={handleOverlayClick}>
+      <div className="modal-content">
+        <div className="modal-header">
+          <span className="modal-close-icon" onClick={onClose}>✖</span> {/* Croix pour fermer */}
+        </div>
+        <p className="modal-message">{message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ModalSuccessMessage;
---
+++ src/lib/index.js
@@ -0,0 +1,2 @@
+// src/lib/index.js
+export { default as ModalSuccessMessage } from './components/SuccessMessage'; // Assurez-vous que le chemin est correct
